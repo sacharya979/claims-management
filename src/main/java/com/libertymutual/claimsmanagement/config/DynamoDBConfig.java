e309f29b1fc6639e050ece507e083389251d6d17
Santosh Acharya <sacharya979@gmail.com>
2023-11-14 20:58:10 +0000

    DynamoDb Config file added along with its required dependencies

---
+++ src/main/java/com/libertymutual/claimsmanagement/config/DynamoDBConfig.java
@@ -0,0 +1,29 @@
+package com.libertymutual.claimsmanagement.config;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DynamoDBConfig {
+
+    @Bean
+    @Primary
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.US_EAST_1) // Set the region to us-east-1
+                .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
+    }
+}
